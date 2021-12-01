c2134392cc6937aa92e79d8d0b4fc7804240dfd8
Brad <brad.cocar@gmail.com>
2021-11-30 19:02:07 -0800

    Added changing board tiles and random turn assignment at start of round

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 const gameboard = (() => {
-    let board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
-    for(i = 0; i < board.length; i++) {
-        const boardTile = document.createElement('div');
-        boardTile.classList.add('tile');
-        boardTile.setAttribute('data-tile', i);
-        boardTile.addEventListener('click', () => playerSelection(boardTile.getAttribute('data-tile')) );
-        document.querySelector('.gameboard').appendChild(boardTile);
-        const tileText = document.createElement('p');
-        tileText.classList.add('tile-text');
-        tileText.textContent = `${board[i]}`;
-        boardTile.appendChild(tileText);
+    let board = ["", "", "", "", "", "", "", "", ""];
+    function updateBoard() {
+        document.querySelectorAll('.tile').forEach(remove => remove.remove());
+        for(i = 0; i < board.length; i++) {
+            const boardTile = document.createElement('div');
+            boardTile.classList.add('tile');
+            boardTile.setAttribute('data-tile', i);
+            document.querySelector('.gameboard').appendChild(boardTile);
+            const tileText = document.createElement('p');
+            tileText.classList.add('tile-text');
+            tileText.textContent = `${board[i]}`;
+            boardTile.appendChild(tileText);
+        }
+        addButtonListeners();
+    };
+    updateBoard();
+    function addButtonListeners() {
+        const boardTile = document.querySelectorAll('.tile');
+        boardTile.forEach(addClick => addClick.addEventListener('click', () => validateSelection(addClick.getAttribute('data-tile'))));
     }
-    function playerSelection(tileNumber) {
-        alert(`${tileNumber} clicked this!`)
+    addButtonListeners();
+    function validateSelection(tileNumber, team) {
+        team = 'X';
+        if(!board[tileNumber]) {
+            board[tileNumber] = team;
+            updateBoard();
+        }
     }
+    return {validateSelection};
 })();
 
-const playerCreator = (side) => {
-    this.side = side;
-    return {side};
+const playerCreator = (team) => {
+    this.team = team;
+    return {team};
 }
 
+const gamehandler = (() => {
+    let turn;
+    (() => {
+        const decideTurn = Math.round(Math.random());
+        if(decideTurn) {
+            turn = 'X';
+        } else if(!decideTurn) {
+            turn = 'O';
+        }
+        alert(turn);
+    })();
+    function changeTurn() {
+
+    }
+    return {changeTurn};
+})();
